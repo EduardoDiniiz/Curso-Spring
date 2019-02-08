735aa2ca21b01f46eff843cc8a6f3fe2439c1212
Eduardo Diniz <eduardodiniz2110@gmail.com>
2019-02-08 12:40:02 -0300

    Projeto criado

---
+++ src/main/java/com/eduardo/estudomc/estudomc/EstudomcApplication.java
@@ -0,0 +1,14 @@
+package com.eduardo.estudomc.estudomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudomcApplication.class, args);
+	}
+
+}
+
